3432b739535a1309c71987ed02591a98429326c8
Stumblinbear <stumblinbear@gmail.com>
2021-04-07 15:02:39 -0500

    Don't purge necessary styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     content: [ './index.html', './src/**/*.{vue,js,ts,jsx,tsx}' ],
 
     options: {
-      safelist: ['w-6', 'h-6', 'w-8', 'h-8'],
+      safelist: [
+        'w-4', 'h-4', 'w-6', 'h-6', 'w-8', 'h-8', 'w-12', 'h-12',
+        'py-5', 'py-7', 'py-2'
+      ],
     }
   },
   darkMode: false, // or 'media' or 'class'
