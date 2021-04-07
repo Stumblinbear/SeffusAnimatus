eecf676a7b843274d8032bb0a20df5526fcefb62
Stumblinbear <stumblinbear@gmail.com>
2021-04-07 07:59:06 -0500

    Analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueGtag, { trackRouter } from "vue-gtag-next";
 import './index.css';
 
 const app = createApp(App);
+
+app.use(VueGtag, {
+    property: {
+      id: "G-SM1JED26GS"
+    }
+});
+
+trackRouter(router, {
+    useScreenview: true
+});
 
 app.use(router);
 
