8d78dc7923c10b87be0562d505de2a387e51b4f3
Stumblinbear <stumblinbear@gmail.com>
2021-05-29 20:13:45 -0500

    Fix collaboration formtting

--- src/views/Collaborate.vue
+++ src/views/Collaborate.vue
@@ -1,26 +1,30 @@
 <template>
-    <div class="w-full p-5 bg-white shadow rounded-lg text-gray-800">
-        <p>
-            Being an animator (I can't draw for crap), I must work with artists regularly to do commissions.
+    <div class="w-full p-5 bg-white shadow rounded-lg text-gray-800 space-y-5">
+        <div class="space-y-2">
+            <p>
+                Being an animator (I can't draw for crap), I must work with artists regularly to do commissions.
 
-            That's great, because I get to meet amazing artists and wonderful people regularly! :)
-        </p>
+                That's great, because I get to meet amazing artists and wonderful people regularly! :)
+            </p>
 
-        <p class="mt-3">
-            That said, I <i>am</i> looking for a art partner to work with regularly.
-        </p>
+            <p>
+                That said, I <i>am</i> looking for a art partner to work with regularly.
+            </p>
 
-        <p class="mt-3">
-            I really want to get into animating comic panels, so if you're an artist who draws these, absolutely hit me up!
-        </p>
+            <p>
+                I really want to get into animating comic panels, so if you're an artist who draws these, absolutely hit me up!
+            </p>
+        </div>
         
-        <s-header class="mt-5" title="What it Entails" />
+        <div class="space-y-2">
+            <s-header title="What it Entails" />
 
-        <s-card>
-            Basically, I need artists that can properly layer art so it can be animated.
-                    
-            This means there is some extra work required at drawing time, however many find this relatively straight-forward with little extra time added to the drawing process.
-        </s-card>
+            <s-card>
+                Basically, I need artists that can properly layer art so it can be animated.
+                        
+                This means there is some extra work required at drawing time, however many find this relatively straight-forward with little extra time added to the drawing process.
+            </s-card>
+        </div>
         
         <contact-me />
     </div>
